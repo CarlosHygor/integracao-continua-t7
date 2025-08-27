72db5649adcd76366a72a8869a75548474aac4c2
Carlos Hygor <carlos.feliix@hotmail.com>
2025-08-26 19:07:57 -0500

    testando dnv

--- sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
@@ -11,6 +11,9 @@ public class SgcmapiApplication {
 
 	public static void main(String[] args) {
 		System.out.println("teste");
+		System.out.println("teste2");
+		System.out.println("teste3");
+		System.out.println("teste4");
 		SpringApplication.run(SgcmapiApplication.class, args);
 	}
 
